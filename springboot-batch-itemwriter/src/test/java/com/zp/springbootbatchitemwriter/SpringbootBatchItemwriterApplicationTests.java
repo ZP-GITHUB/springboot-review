44708906b9519a35430b5f46cc084d1ee735204b
ZP <991475623@qq.com>
2021-04-17 23:59:35 +0800

    update

---
+++ springboot-batch-itemwriter/src/test/java/com/zp/springbootbatchitemwriter/SpringbootBatchItemwriterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zp.springbootbatchitemwriter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBatchItemwriterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
