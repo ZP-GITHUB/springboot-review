e99eac629bf2cb51003eb66d959656b0e2ae468b
ZP <991475623@qq.com>
2021-04-18 00:26:11 +0800

    update

--- springboot-batch-itemwriter/src/main/java/com/zp/springbootbatchitemwriter/reader/ItemReaderConfigure.java
+++ springboot-batch-itemwriter/src/main/java/com/zp/springbootbatchitemwriter/reader/ItemReaderConfigure.java
@@ -19,13 +19,13 @@ public class ItemReaderConfigure {
     public ListItemReader<TestData> simpleReader() {
         List<TestData> data = new ArrayList<>();
         TestData testData1 = new TestData();
-        testData1.setId(1);
+        testData1.setId(7);
         testData1.setField1("11");
         testData1.setField2("12");
         testData1.setField3("13");
         data.add(testData1);
         TestData testData2 = new TestData();
-        testData2.setId(2);
+        testData2.setId(8);
         testData2.setField1("21");
         testData2.setField2("22");
         testData2.setField3("23");
