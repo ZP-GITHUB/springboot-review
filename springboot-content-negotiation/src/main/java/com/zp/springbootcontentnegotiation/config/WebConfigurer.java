d588ff2c3f002783a5199eb44a8de16c0946c328
ZP <991475623@qq.com>
2021-04-27 21:41:13 +0800

    update

--- springboot-content-negotiation/src/main/java/com/zp/springbootcontentnegotiation/config/WebConfigurer.java
+++ springboot-content-negotiation/src/main/java/com/zp/springbootcontentnegotiation/config/WebConfigurer.java
@@ -1,10 +1,18 @@
 package com.zp.springbootcontentnegotiation.config;
 
 import com.zp.springbootcontentnegotiation.converter.PropertiesHttpMessageConverter;
+import com.zp.springbootcontentnegotiation.handler.PropertiesHandlerMethodReturnValueHandler;
+import com.zp.springbootcontentnegotiation.resolver.PropertiesHandlerMethodArgumentResolver;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.HandlerMethodReturnValueHandler;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
 
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,5 +29,31 @@ public class WebConfigurer implements WebMvcConfigurer {
         // converters.add(new PropertiesHttpMessageConverter());
         // 指定顺序，这里为第一个
         converters.add(0, new PropertiesHttpMessageConverter());
+    }
+
+    @Autowired
+    private RequestMappingHandlerAdapter requestMappingHandlerAdapter;
+
+    @PostConstruct
+    public void init() {
+        // 获取当前 RequestMappingHandlerAdapter 所有的 ArgumentResolver对象
+        List<HandlerMethodArgumentResolver> argumentResolvers = requestMappingHandlerAdapter.getArgumentResolvers();
+        List<HandlerMethodArgumentResolver> newArgumentResolvers = new ArrayList<>(argumentResolvers.size() + 1);
+        // 添加 PropertiesHandlerMethodArgumentResolver 到集合第一个位置
+        newArgumentResolvers.add(0, new PropertiesHandlerMethodArgumentResolver());
+        // 将原 ArgumentResolver 添加到集合中
+        newArgumentResolvers.addAll(argumentResolvers);
+        // 重新设置 ArgumentResolver对象集合
+        requestMappingHandlerAdapter.setArgumentResolvers(newArgumentResolvers);
+
+        // 获取当前 RequestMappingHandlerAdapter 所有的 returnValueHandlers对象
+        List<HandlerMethodReturnValueHandler> returnValueHandlers = requestMappingHandlerAdapter.getReturnValueHandlers();
+        List<HandlerMethodReturnValueHandler> newReturnValueHandlers = new ArrayList<>(returnValueHandlers.size() + 1);
+        // 添加 PropertiesHandlerMethodReturnValueHandler 到集合第一个位置
+        newReturnValueHandlers.add(0, new PropertiesHandlerMethodReturnValueHandler());
+        // 将原 returnValueHandlers 添加到集合中
+        newReturnValueHandlers.addAll(returnValueHandlers);
+        // 重新设置 ReturnValueHandlers对象集合
+        requestMappingHandlerAdapter.setReturnValueHandlers(newReturnValueHandlers);
     }
 }
--- springboot-content-negotiation/src/main/java/com/zp/springbootcontentnegotiation/controller/TestController.java
+++ springboot-content-negotiation/src/main/java/com/zp/springbootcontentnegotiation/controller/TestController.java
@@ -1,7 +1,9 @@
 package com.zp.springbootcontentnegotiation.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Properties;
@@ -10,11 +12,18 @@ import java.util.Properties;
  * @author ZP
  * @date 2021/4/26.
  */
-@RestController
+//@RestController
+@Controller
 public class TestController {
 
     @GetMapping(value = "test", consumes = "text/properties")
     public Properties test(@RequestBody Properties properties) {
+        return properties;
+    }
+
+    @GetMapping(value = "test1", consumes = "text/properties")
+//    @ResponseBody
+    public Properties test1(Properties properties) {
         return properties;
     }
 }
